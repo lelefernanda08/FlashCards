c7ed2186d194ded5a96cee5315a4b7a945aa1b52
Letícia Schmitz <137583118+lelefernanda08@users.noreply.github.com>
2024-10-29 08:31:32 -0300

    29/10

---
+++ perguntas.js
@@ -0,0 +1,61 @@
+criaCartao(
+    'BIOLOGIA',
+'O que é Seleção Natural?',
+'O organismo mais apto sobrevive e passa suas características aos descendentes, garantindo, portanto, que características vantajosas fixem-se em uma população.'
+)
+    
+criaCartao(
+    'GEOGRAFIA',
+    'O que é desemprego conjuntural?',
+    'É o desemprego causado por uma crise econômica.'
+)
+criaCartao(
+    'HISTÓRIA',
+    'Quais eram os países que fa0ziam parte da Tríplice Aliança?',
+    'Alemanha, Áustria-Hungria e Itália.'
+)
+criaCartao(
+    'LÍNGUA PORTUGUESA',
+    'O que é uma crônica?',
+'A crônica é um gênero textual caracterizado por textos curtos, de linguagem simples que retrata os aspectos da vida cotidiana, geralmente com toques de humor ou ironia.'
+)
+criaCartao(
+    'QUÍMICA',
+    'O que são hidrocarbonetos?',
+    'São um grupo de substâncias orgânicas que somente possuem carbono e hidrogênio na sua composição.'
+)
+criaCartao(
+    'EMPREENDEDORISMO',
+    'Qual é o tempo máximo de contrato de um estagiário em uma mesma empresa?',
+    '2 anos, exceto quando se trata de um estagiário portador de deficiência, que pode permanecer até a conclusão do curso.'
+)
+criaCartao(
+    'BIOTECNOLOGIA',
+    'O que é um alelo dominante?',
+    'Os alelos dominantes são aqueles que, mesmo em dose simples, conseguem se manifestar. Ou seja, quando um alelo dominante está presente, o seu fenótipo será expressado.'
+)
+criaCartao(
+    'SOCIOLOGIA',
+    'O que é comunicação assertiva?',
+    'É a comunicação que consegue passar as informações com clareza, dinâmica e respeito, obtendo o retorno esperado.'
+)
+criaCartao(
+    'MATEMÁTICA',
+    'Qual a fórmula para calcular a tangente?',
+    'É o cateto oposto dividido pelo cateto adjacente ao ângulo de interesse.'
+)
+criaCartao(
+    'PROGRAMAÇÃO',
+    'O que é uma div?',
+    'São elementos usados para definir partes de um documento.'
+)
+criaCartao(
+    'LÍNGUA INGLESA',
+    'Para que é usado o “Will”?',
+    'O will é usado quando falamos sobre algo no futuro e costuma estar acompanhado de expressões que indicam incerteza.'
+)
+criaCartao(
+    'ENTRETENIMENTO',
+    'Quais são os 3 personagens principais do filme “Harry Potter”?',
+    'Harry Potter, Rony Weasley e Hermione Granger.'
+)
